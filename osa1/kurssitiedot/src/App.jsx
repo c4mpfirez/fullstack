aa3b53f5927df22467aa2f009ce8fe5d3c958278
c4mpfirez <jonbon96@hotmail.com>
2023-11-26 16:12:02 +0200

    fix

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -16,47 +16,50 @@ const App = () => {
       }
     ]
   }
-      const Header = () => { 
-        return (
-          <div>
-            <h1>{course.name}</h1>
-          </div>
-        )
-      }
 
-      const Content = ({ parts }) => {
-        const Part = (props) => {
-          console.log(props)
-          return (
-            <div>
-              <p>{props.part} - {props.exercises}</p>
-            </div>
-          )
-        }
-        return (
-          <div>
-            <Part part={parts[0].name} exercises={parts[0].exercises} />
-            <Part part={parts[1].name} exercises={parts[1].exercises} />
-            <Part part={parts[2].name} exercises={parts[2].exercises} />
-          </div>
-        )
-      }
+  const Header = ({ course }) => {
+    return (
+      <div>
+        <h1>{course}</h1>
+      </div>
+    )
+  }
 
-      const Total = () => {
-        console.log(course.parts)
-        return (
-          <div>
-            <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
-          </div>
-        )
-      }
+  const Content = ({ parts }) => {
+    const Part = (props) => {
+      console.log(props)
       return (
         <div>
-          <Header/>
-          <Content parts={course.parts}/>
-          <Total/>
+          <p>{props.part} - {props.exercises}</p>
         </div>
       )
     }
+    return (
+      <div>
+        <Part part={parts[0].name} exercises={parts[0].exercises} />
+        <Part part={parts[1].name} exercises={parts[1].exercises} />
+        <Part part={parts[2].name} exercises={parts[2].exercises} />
+      </div>
+    )
+  }
 
-    export default App
+  const Total = ({ parts }) => {
+    console.log(parts)
+    const Allexercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+    return (
+      <div>
+        <p>Number of exercises {Allexercises}</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
